64520c354ec980f2f948be66ed96b65003ad2991
Scott Eisenberg <seisenberg2015@gmail.com>
2026-02-14 15:28:43 -0800

    Cal Poly only emails functionality added

--- frontend/app/login.tsx
+++ frontend/app/login.tsx
@@ -14,6 +14,7 @@ import {
 import { useRouter } from 'expo-router';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import {supabase} from '@/lib/supabase';
 
 export default function LoginScreen() {
   const router = useRouter();
@@ -38,9 +39,9 @@ export default function LoginScreen() {
       return;
     }
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      setError('Please enter a valid email');
+    const calPolyEmailRegex = /^[^\s@]+@calpoly\.edu$/i;
+    if (!calPolyEmailRegex.test(email)) {
+      setError('Please enter a valid Cal Poly email (@calpoly.edu)');
       return;
     }
 
