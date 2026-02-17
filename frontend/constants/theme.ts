291cea868e6cad4dba3bd9524d6b2edcd69fc366
Arin Johar <67350082+arinjohar@users.noreply.github.com>
2026-02-17 12:44:27 -0800

    feat: updated nav bar (#25)
    * feat: updated nav bar
    
    * fix: made a few corrections on the nav bar look
    
    * fix: fixed navbar coloring
    
    ---------
    
    Co-authored-by: Arin Johar <johararin.com>

--- frontend/constants/theme.ts
+++ frontend/constants/theme.ts
@@ -12,6 +12,7 @@ export const Colors = {
   light: {
     text: '#11181C',
     background: '#fff',
+    tabBarBackground: '#F5F5F5', 
     tint: "#174735",
     icon: '#687076',
     tabIconDefault: '#687076',
@@ -20,6 +21,7 @@ export const Colors = {
   dark: {
     text: '#ECEDEE',
     background: '#151718',
+    tabBarBackground: '#0E0F10', 
     tint: "#174735",
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
