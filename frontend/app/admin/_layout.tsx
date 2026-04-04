e05f5e2f77d30e42da9988df29afcf156c2d8b47
Peter Chinh <peterchinh1018@gmail.com>
2026-04-03 20:14:51 -0700

    feat: admin page and verification

---
+++ frontend/app/admin/_layout.tsx
@@ -0,0 +1,34 @@
+import { supabase } from "@/lib/supabase";
+import { Slot, useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+
+export default function AdminLayout() {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkRole = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      const { data } = await supabase
+        .from("user_roles")
+        .select("role")
+        .eq("user_id", user?.id)
+        .single();
+
+      if (data?.role !== "admin") {
+        router.replace("/");
+      } else {
+        setLoading(false);
+      }
+    };
+
+    checkRole();
+  }, []);
+
+  if (loading) return null; // Or a loading spinner
+
+  return <Slot />; // This renders the actual admin page if allowed
+}
