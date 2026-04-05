1f10ee170e02163f231a48bff884087a373d416e
Stella <109620913+stellad22@users.noreply.github.com>
2026-04-05 02:02:32 -0700

    centered logo on landing page (#60)

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -41,9 +41,9 @@ export default function HomeScreen() {
   }
   return (
     <ThemedView style={[styles.container, { backgroundColor }]}>
-      <PolyLogo />
-
-      <View style={{ flex: 1 }} />
+      <View style={styles.logoContainer}>
+        <PolyLogo />
+      </View>
 
       <ThemedView style={styles.descriptionContainer}>
         <ThemedText
@@ -90,6 +90,11 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+  },
+  logoContainer: {
+    flex: 2,
+    justifyContent: "center",
     alignItems: "center",
   },
   header: {
